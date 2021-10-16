29dc81090dd43668aed1e6135856e97cea6e7ec4
Jerome Celle <jeffyer38130@gmail.com>
2021-10-16 17:49:50 +0200

    add axios and first login test

--- tests/sum.test.ts
+++ tests/sum.test.ts
@@ -1,5 +1,18 @@
-import { sum } from '../src';
+import { axiosGetTest, sum } from '../src';
+import DoneCallback = jest.DoneCallback;
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
+});
+
+test('check axios get', (done: DoneCallback) => {
+  axiosGetTest().subscribe({
+    next: (data: any) => {
+      expect(data.data.statusCode).toBe(200);
+    },
+    error: (data) => {
+      console.log(data);
+    },
+    complete: () => done(),
+  });
 });
